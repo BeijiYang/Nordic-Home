007a1d5f6c3aa8c15bccb64fd383a6e7e688dd1f
BeijiYang <langyuyang@outlook.com>
2016-10-11 19:55:55 +0800

    MINA demo

---
+++ pages/work/work.js
@@ -0,0 +1,16 @@
+Page({
+  data: {
+    items: [
+        { imgUrls: '../../assets/img1.png'},
+        { imgUrls: '../../assets/img2.png'},
+        { imgUrls: '../../assets/img3.png'},
+    ],
+    indicatorDots: false,
+    autoplay: true,
+    interval: 2000,
+    duration: 1000,
+    btnText: "预约设计师",
+    introduction: "左右拖动以选择"
+  },
+ 
+})
