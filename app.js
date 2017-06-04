b9fcc4fa5940f3f1de77b8e82ec03e1727734cad
BeijiYang <langyuyang@outlook.com>
2017-06-05 00:46:08 +0800

    add login with openid

--- app.js
+++ app.js
@@ -1,10 +1,37 @@
 //app.js
 App({
   onLaunch: function () {
+    console.log("onlunch")
+    wx.login({
+      success: function (res) {
+        if (res.code) {
+          console.log(res.code)
+          //发起网络请求
+          wx.request({
+            url: 'http://localhost:3000/login',
+            method: 'POST',
+            data: {
+              code: res.code
+            },
+            header: {
+              'content-type': 'application/json'
+            },
+            success: res => {
+              wx.setStorageSync('token', res.data.token)
+            }
+            // success: function(res) {
+            //   console.log(res.data)
+            // }
+          })
+        } else {
+          console.log('获取用户登录态失败！' + res.errMsg)
+        }
+      }
+    });
     //调用API从本地缓存中获取数据
-    var logs = wx.getStorageSync('logs') || []
-    logs.unshift(Date.now())
-    wx.setStorageSync('logs', logs)
+    // var logs = wx.getStorageSync('logs') || []
+    // logs.unshift(Date.now())
+    // wx.setStorageSync('logs', logs)
   },
   getUserInfo:function(cb){
     var that = this;
--- pages/logs/logs.js
+++ pages/logs/logs.js
@@ -1,14 +1,78 @@
 //logs.js
 var util = require('../../utils/util.js')
+var app = getApp()
 Page({
   data: {
-    logs: []
+    logs: [],
+    motto: "",
+    userInfo: {},
+    btnText: "扫码",
+    account:"已预约设计师："
   },
-  onLoad: function () {
-    this.setData({
-      logs: (wx.getStorageSync('logs') || []).map(function (log) {
-        return util.formatTime(new Date(log))
-      })
+  bindViewTap: function () {
+    wx.navigateTo({
+      url: '../logs/logs'
     })
+  },
+  scanCode: function () {
+    wx.scanCode({
+      success: (res) => {
+        console.log(res)
+      }
+    })
+  },
+  
+  onLoad: function () {
+    var that = this
+    const token = wx.getStorageSync('token')
+    wx.request({
+      url: 'http://localhost:3000/user/orders',
+      header: {
+        'Authorization': token
+      },
+      success: function (res) {
+        console.log("获取订单: "),
+          console.log(res.data),
+          that.setData({
+          motto: res.data.orders[0]
+          })
+      },
+      fail: error => {
+        console.log(error)
+      }
+    }) 
+
+    // this.setData({
+      
+      // logs: (wx.getStorageSync('logs') || []).map(function (log) {
+      //   return util.formatTime(new Date(log))
+      // })
+    // }),
+
+      //调用应用实例的方法获取全局数据
+      app.getUserInfo(function (userInfo) {
+        //更新数据
+        that.setData({
+          userInfo: userInfo
+        })
+      })
   }
 })
+
+
+
+
+//logs.js
+// var util = require('../../utils/util.js')
+// Page({
+//   data: {
+//     logs: []
+//   },
+//   onLoad: function () {
+//     this.setData({
+//       logs: (wx.getStorageSync('logs') || []).map(function (log) {
+//         return util.formatTime(new Date(log))
+//       })
+//     })
+//   }
+// })
